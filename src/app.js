a113cd2b05e6e30e9af26150ae6cae844afd3a03
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-03-01 00:52:16 +0530

    Finalised the app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import Footer from "./components/Footer";
 
 const AppLayout = () => {
   return (
     <div>
       <Header />
       <Body />
-      <Footer />
     </div>
   );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,21 @@ const Card = (props) => {
   const { name, price, image, rating } = data;
   const avg = rating.average;
   return (
-    <div className="m-4 p-4 w-[200px] bg-gray-100 rounded-lg shadow-lg">
-      <img className="h-auto" src={image} alt="image" />
+    <div className="m-4 p-4 w-[220px] bg-white rounded-lg shadow-lg">
+      <div className="text-center">
+        <img
+          className="h-45"
+          src={image}
+          onError={(e) =>
+            (e.target.src =
+              "https://www.shutterstock.com/image-vector/no-image-available-picture-coming-600nw-2057829641.jpg")
+          }
+        />
+      </div>
       <div className="space-y-2">
-        <h3 className="font-bold line-clamp-1">{name}</h3>
+        <h3 className="font-bold text-lg line-clamp-1">{name}</h3>
         <h4>{price}</h4>
-        <h4>{Math.round(avg * 10) / 10} stars</h4>
+        <h4>{Math.round(avg * 10) / 10} ⭐</h4>
         <h4>{rating.reviews} Reviews</h4>
       </div>
     </div>
