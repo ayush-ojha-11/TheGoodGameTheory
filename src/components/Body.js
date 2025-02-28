58392afec0c253f0268ffae9353da1e67c874899
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-02-28 20:23:52 +0530

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+const Body = () => {
+  return (
+    <div>
+      <h1>From Body</h1>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="p-5 text-center bg-amber-700">
+      <h1 className="text-3xl font-mono text-cyan-50">The GoodGame Theory</h1>
+    </div>
+  );
+};
+
+export default Header;
